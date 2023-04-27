50e9c1bbc6db4bbd15d54ed75095c5f5579873bb
wangsf <wangshaofu.it@ciic.com.cn>
2023-04-27 23:50:21 +0800

    集成自定义负载策略

--- src/main/java/com/kyxs/cloud/gateway/GatewayApplication.java
+++ src/main/java/com/kyxs/cloud/gateway/GatewayApplication.java
@@ -1,19 +1,21 @@
 package com.kyxs.cloud.gateway;
 
+import com.kyxs.cloud.loadbalance.config.CustomizeLoadBalancerRuleConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import java.text.SimpleDateFormat;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
+@Slf4j
+@LoadBalancerClients(defaultConfiguration = CustomizeLoadBalancerRuleConfiguration.class) //自定义负载均衡策略
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(System.currentTimeMillis())+" [main] SUCCESSFUL com.kyxs.cloud.gatewat.GatewayApplication - 网关服务 kyxs-cloud-gateway:7900 启动成功！");
-
+		log.info(" SUCCESSFUL com.kyxs.cloud.gateway.GatewayApplication - 网关服务 kyxs-cloud-gateway:9000 启动成功！");
 	}
-
 }
